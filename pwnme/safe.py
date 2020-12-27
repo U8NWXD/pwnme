cfff64df4d7b8a375711b22ccb7ee1c0bfef73ee
cs <cs.temporary@icloud.com>
2020-12-27 00:45:52 +0000

    Add security headers

--- pwnme/safe.py
+++ pwnme/safe.py
@@ -106,6 +106,54 @@ def withdraw():
         new_balance = balance - amount
         base.update_balance(new_balance)
         return redirect(url_for('safe.index'))
-    elif not form.validate():
+    if request.method == 'POST':
         flash(form.errors)
     return render_template('withdraw_safe.html', form=form)
+
+
+@bp.after_request
+def add_response_headers(response):
+    '''Set response headers for security.
+
+    Headers set:
+
+    * ``Strict-Transport-Security``: Once a browser connects to you over
+      HTTPS, it will require HTTPS connections for the next 2 years.
+      This prevents an attacker from sending users to a fake HTTP
+      version of your site. The ``preload`` option will get your site
+      included in lists of HTTPS-only sites shipped with major browsers,
+      so users will be safe even if they've never visited your site.
+    * ``X-Content-Type-Options``: Tell browsers not to sniff responses
+      to try and figure out what the content-type is. This sniffing can
+      lead to vulnerabilities when the browser and your firewall use a
+      different content-type. For example, if XSS code only appears when
+      using the content-type sniffed by the browser, the firewall won't
+      block the XSS code like it should
+    * ``X-Frame-Options``: Prevent someone else from putting your site
+      in an iframe. This is important because if someone puts your site
+      in an iframe on a malicious site, users visiting the malicious
+      site could be tricked into performing actions on your site.
+    * ``X-XSS-Protection``: Tell the browser to block any JavaScript
+      that is present in both the response and the request. This blocks
+      reflected XSS, though it is unnecessary in modern browsers when
+      you use a strict content security policy. It's still helpful for
+      older browsers, though.
+    * ``Content-Security-Policy``: Restrict where the browser can load
+      resources for your page. We use a very strict policy here that
+      blocks loading all content except styles, which can be loaded from
+      our site only. Such a strict policy also blocks nearly all XSS
+      attacks. You can check your policy using
+      `Google's CSP Evaluator <https://csp-evaluator.withgoogle.com/>`_.
+
+    You can check `Mozilla's documentation
+    <https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers>`_ for
+    details on these headers and others.`
+    '''
+    response.headers['Strict-Transport-Security'] = (
+        'max-age=63072000; includeSubDomains; preload')
+    response.headers['X-Content-Type-Options'] = 'nosniff'
+    response.headers['X-Frame-Options'] = 'SAMEORIGIN'
+    response.headers['X-XSS-Protection'] = '1; mode=block'
+    response.headers['Content-Security-Policy'] = (
+        "default-src 'none'; style-src 'self'")
+    return response
